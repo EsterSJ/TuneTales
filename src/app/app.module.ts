d1dbf67c92bb882305688f0df3423d04e5b37121
Marcelcoder001 <marcel.soto324@gmail.com>
2023-07-25 19:11:01 +0100

    nuevo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormRegisterComponent } from './components/form-register/form-register.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsCardComponent } from './components/events-card/events-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsCardComponent,
+    FormRegisterComponent
   ],
   imports: [
     BrowserModule,
