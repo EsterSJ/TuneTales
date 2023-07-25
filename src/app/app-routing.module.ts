051c5e90f47b44b735ca0f173432663a245c6356
thomaseade <eadethomas95@gmail.com>
2023-07-25 14:45:56 +0200

    crear evento en camino

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../../src/app/pages/login/login.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
 
 const routes: Routes = [
+  { path: 'create-event', component: CreateEventComponent },
   { path: 'login', component: LoginComponent },
+  
 
   { path: '', redirectTo: '/login', pathMatch: 'full' }, 
 ];
--- src/app/pages/create-event/create-event.component.ts
+++ src/app/pages/create-event/create-event.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.css']
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit {
+  createForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm = this.formBuilder.group({
+      eventName: ['', Validators.required],
+      image: ['', Validators.required],
+      location: ['', Validators.required],
+      date: ['', Validators.required],
+      time: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  onImageSelected(event: Event) {
+   
+  }
+
+  onSubmit() {
+   
+  }
 }
