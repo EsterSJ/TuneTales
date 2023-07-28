3b5b99d7946373d76c9bba9dc0c5c43d40e8f21e
EsterSJ <ester_s@hotmail.com>
2023-07-28 12:25:55 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../../src/app/pages/login/login.component';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormRegisterComponent } from './pages/form-register/form-register.component';
+import { ForYouComponent } from './pages/for-you/for-you.component';
+import { AddMusicComponent } from './pages/add-music/add-music.component';
+import { CommunityComponent } from './pages/community/community.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: 'register', component: FormRegisterComponent },
   { path: 'create-event', component: CreateEventComponent },
   { path: 'login', component: LoginComponent },
-  // { path: '', redirectTo: '/login', pathMatch: 'full' }, 
-
-
-  { path: '', redirectTo: '/edit-profile', pathMatch: 'full' }, 
+  { path: 'paraTi', component: ForYouComponent },
+  { path: 'addMusic', component: AddMusicComponent },
+  { path: 'community', component: CommunityComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
