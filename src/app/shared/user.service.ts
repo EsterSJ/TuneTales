820b03fb3bb3932f0d8c3668121925305807da78
Marcelcoder001 <marcel.soto324@gmail.com>
2023-08-03 11:51:51 +0100

    funcionRegister

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+Observable
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +12,14 @@ export class UserService {
   private url: string = "http:localhost:4200";
 
   public logueado:boolean = false;
-  public user: UserService;
+  public user: User;
 
-  constructor() { 
+  constructor(private http:HttpClient) { 
 
     this.logueado = false;
   }
+
+  register(user:User):Observable<Object>{
+    return this.http.post(this.url + "register", user)
+}
 }
