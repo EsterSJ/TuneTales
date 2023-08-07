8bb20410c1de1f8df675e623f992b800b3aff068
ivanapizzo <ivb.pizzo@gmail.com>
2023-08-07 13:38:26 +0200

    subiendo cambios para instalar sweet alert sin conflictos

--- src/app/components/add-music-form/add-music-form.component.ts
+++ src/app/components/add-music-form/add-music-form.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Publicacion } from 'src/app/models/publicacion';
+import { PublicationService } from 'src/app/shared/publication.service';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-add-music-form',
@@ -9,8 +13,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AddMusicFormComponent {
 
     public addMusicForm: FormGroup;
+    public publiacion: Publicacion;
 
-    constructor (private formBuilder: FormBuilder){
+    constructor (private formBuilder: FormBuilder, private router: Router, public publicationService:PublicationService, private UserService:UserService){
       this.buildForm();
     }
 
@@ -21,5 +26,24 @@ export class AddMusicFormComponent {
         letra: '',
         historia: ''
       });
+    }
+
+
+    addPublication(id_publicacion: number, id_user: number, link_soundCloud: string, letter: string, history: string, likes: number) {
+
+      let publicacion = new Publicacion(
+        this.publicationService.publicacion.id_publicacion,
+        this.UserService.user.id_user,
+        link_soundCloud,
+        letter,
+        history,
+        likes
+      );
+
+      this.publicationService.postPublication(publicacion).subscribe((data:Publicacion)=> {
+        this.publiacion = data;
+        console.log(data);
+        this.router.navigateByUrl('/Publicacion')
+      })
     }
 }
