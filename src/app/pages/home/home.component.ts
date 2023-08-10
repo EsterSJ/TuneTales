db8e99794f4350202b08b7198fcd9e7840a07aac
thomaseade <eadethomas95@gmail.com>
2023-08-10 17:50:00 +0200

    publi-card y carrousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Top3PublicacionesService } from 'src/app/shared/publication.service'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  top3Publicaciones: any[] = []; // Aquí almacenarás las publicaciones
 
+  constructor(private top3Service: Top3PublicacionesService) { }
+
+  ngOnInit() {
+    this.top3Service.getTop3Publicaciones().subscribe(
+      (data: any) => {
+        this.top3Publicaciones = data;
+      });
+  }
 }
--- src/app/shared/publication.service.ts
+++ src/app/shared/publication.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PublicationService {
 
-  constructor() { }
+
+export class Top3PublicacionesService {
+  private apiUrl = 'http://localhost:3000/top3publicaciones';
+
+  constructor(private http: HttpClient) { }
+
+  getTop3Publicaciones() {
+    return this.http.get(this.apiUrl);
+  }
 }
