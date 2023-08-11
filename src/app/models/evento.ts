79d32c699697bb58feb01013646e1e45b033e733
Marcelcoder001 <marcel.soto324@gmail.com>
2023-08-11 09:20:41 +0100

    ok

--- src/app/models/evento.ts
+++ src/app/models/evento.ts
@@ -1,20 +1,27 @@
+import { Time } from "@angular/common";
+
 export class Evento {
 
     public id_event: Number;
     public id_user: Number;
     public name_event: String;
     public date: Date;
-    // public hour: Time; ?????
+    public hour: Time; 
     public place: String;
     public tickets: String;
+    public photo: String;
+    public description: String;
 
-    constructor(id_event: Number, id_user: Number, name_event: String, date: Date, place: String, tickets: String){
+    constructor(id_event: Number, id_user: Number, name_event: String, date: Date, hour: Time, place: String, tickets: String, photo:String, description:String){
         this.id_event = id_event;
         this.id_user = id_user;
         this.name_event = name_event;
         this.date = date;
+        this.hour = hour;
         this.place = place;
         this.tickets = tickets;
+        this.photo = photo;
+        this.description = description;
     }
 
 }
