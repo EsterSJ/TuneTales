d35eecf39e16a77e72bbaad2272429ed51955681
EsterSJ <ester_s@hotmail.com>
2023-09-11 12:02:38 +0200

    despliegue vercel

--- src/app/shared/publication.service.ts
+++ src/app/shared/publication.service.ts
@@ -11,7 +11,7 @@ import { UserService } from './user.service';
 export class PublicationService {
 
   public addMusicForm: FormGroup;
-  private url: string = 'http://api-tune-tales-mgn163wti-marcelcoder01.vercel.app/publicacion';
+  private url: string = 'https://api-tune-tales-mgn163wti-marcelcoder01.vercel.app/publicacion';
   public publicaciones: Publicacion [] = [];
   public publicacion: Publicacion;
   public letra:boolean = false;
@@ -49,7 +49,7 @@ export class PublicationService {
   }
 
   getTop3Publicaciones() {
-    return this.http.get('http://api-tune-tales-mgn163wti-marcelcoder01.vercel.app/top3publicaciones');
+    return this.http.get('https://api-tune-tales-mgn163wti-marcelcoder01.vercel.app/top3publicaciones');
   }
 
 }
