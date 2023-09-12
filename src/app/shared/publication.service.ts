d4a98731434bd4d591f272345cb722ef4866909d
thomaseade <eadethomas95@gmail.com>
2023-09-12 11:06:56 +0200

    arreglos grabacion

--- src/app/shared/publication.service.ts
+++ src/app/shared/publication.service.ts
@@ -49,7 +49,7 @@ export class PublicationService {
   }
 
   getTop3Publicaciones() {
-    return this.http.get('http://localhost:3000');
+    return this.http.get('http://localhost:3000/top3publicaciones');
   }
 
 }
